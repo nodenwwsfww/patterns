a67fdb6e7c8ec4ac234cb519ad3e294673844715
Roman Radchenko <nodenwwsfww@gmail.com>
2024-06-10 16:44:40 +0300

    Update

--- index.ts
+++ index.ts
@@ -4,15 +4,12 @@ import { LoggingDecorator } from './LoggingDecorator';
 import { ErrorHandlingDecorator } from './ErrorHandlingDecorator';
 import { IPaymentService } from './IPaymentService';
 
-// Set up the third-party payment service
 const thirdPartyService = new ThirdPartyPaymentService();
 const paymentAdapter: IPaymentService = new PaymentAdapter(thirdPartyService);
 
-// Decorate the adapter with logging and error handling
 const loggingPaymentService: IPaymentService = new LoggingDecorator(paymentAdapter);
 const errorHandledPaymentService: IPaymentService = new ErrorHandlingDecorator(loggingPaymentService);
 
-// Use the decorated service to make a payment
 (async () => {
     await errorHandledPaymentService.pay(100);
 })();
